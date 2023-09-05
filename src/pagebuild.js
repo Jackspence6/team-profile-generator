a568e4554639b456645d85d6671d85bf6415dcdb
Jackspence6 <jackspence123456@gmail.com>
2023-09-05 15:49:39 +0200

    Added html code for page build to constant.

---
+++ src/pagebuild.js
@@ -0,0 +1,53 @@
+/******************************************/
+/* External dependencies */
+/******************************************/
+
+/******************************************/
+/* Global variables and constants */
+/******************************************/
+// Adding html code for page to constant
+const pageBuild = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/css/bootstrap.min.css"
+      crossorigin="anonymous"
+    />
+    <title>My Team</title>
+  </head>
+  <body>
+    <header
+      class="p-3 mb-2 bg-danger bg-gradient text-white border-bottom border-black border-3"
+    >
+      <h1 class="text-center">My Team</h1>
+    </header>
+    <div class="container mt-4">
+      <div class="row row-cols-1 row-cols-md-3 g-4" id="card-container">
+      </div>
+    </div>
+  </body>
+</html>
+`;
+/******************************************/
+/* Function and class declarations */
+/******************************************/
+
+/******************************************/
+/* Event listeners */
+/******************************************/
+
+/******************************************/
+/* Document manipulation */
+/******************************************/
+
+/******************************************/
+/* Initialization code */
+/******************************************/
+
+/******************************************/
+/* Main logic */
+/******************************************/
