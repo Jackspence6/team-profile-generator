98f61fbe9e79a1f59a8d561ac7f018f5a81e3201
Jackspence6 <jackspence123456@gmail.com>
2023-09-05 17:09:02 +0200

    Updated ManagerCard Build.

--- src/cardbuild.js
+++ src/cardbuild.js
@@ -6,19 +6,30 @@
 /* Global variables and constants */
 /******************************************/
 // Card Building code for employee cards
-const cardBuild = `
-   <!-- Card  -->
-    <div class="card m-0 p-0 rounded" style="width: 18rem;">
+const managerCardBuild = `
+    <!-- Manager Card  -->
+    <div class="card m-0 p-0 rounded" style="width: 18rem">
       <!-- Div to hold both h5 and h6 with the same blue background -->
-      <div class="bg-primary text-white m-0 p-2" style="border-bottom: 5px solid black;">
-        <h5 class="mb-0 text-center">Card title</h5>
-        <h6 class="mb-0 text-center">Card title</h6>
+      <div
+        class="bg-primary text-white m-0 p-2"
+        style="border-bottom: 5px solid black"
+      >
+        <h5 class="mb-0 text-center">Employee Name</h5>
+        <h6 class="mb-0 text-center">Employee Title</h6>
       </div>
       <!-- Separate card to hold the list items -->
       <ul class="list-group list-group-flush m-0 p-4 border border-black">
-        <li class="list-group-item border border-secondary-subtle rounded-top">An item</li>
-        <li class="list-group-item border border-secondary-subtle">A second item</li>
-        <li class="list-group-item border border-secondary-subtle rounded-bottom">A third item</li>
+        <li class="list-group-item border border-secondary-subtle rounded-top">
+          ID:
+        </li>
+        <li class="list-group-item border border-secondary-subtle">
+          Email: <a href="mailto: "></a>
+        </li>
+        <li
+          class="list-group-item border border-secondary-subtle rounded-bottom"
+        >
+          Office Number:
+        </li>
       </ul>
     </div>
 `;
