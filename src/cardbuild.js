6e5db1322ac5173da6780a2fa3b98d435614eb81
Jackspence6 <jackspence123456@gmail.com>
2023-09-05 17:02:17 +0200

    Added card building code to file.

--- src/cardbuild.js
+++ src/cardbuild.js
@@ -5,7 +5,23 @@
 /******************************************/
 /* Global variables and constants */
 /******************************************/
-
+// Card Building code for employee cards
+const cardBuild = `
+   <!-- Card  -->
+    <div class="card m-0 p-0 rounded" style="width: 18rem;">
+      <!-- Div to hold both h5 and h6 with the same blue background -->
+      <div class="bg-primary text-white m-0 p-2" style="border-bottom: 5px solid black;">
+        <h5 class="mb-0 text-center">Card title</h5>
+        <h6 class="mb-0 text-center">Card title</h6>
+      </div>
+      <!-- Separate card to hold the list items -->
+      <ul class="list-group list-group-flush m-0 p-4 border border-black">
+        <li class="list-group-item border border-secondary-subtle rounded-top">An item</li>
+        <li class="list-group-item border border-secondary-subtle">A second item</li>
+        <li class="list-group-item border border-secondary-subtle rounded-bottom">A third item</li>
+      </ul>
+    </div>
+`;
 /******************************************/
 /* Function and class declarations */
 /******************************************/
