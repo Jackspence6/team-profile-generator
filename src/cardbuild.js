550772904e0bcbf315c3f90c0de630ecd1f7cd18
Jackspence6 <jackspence123456@gmail.com>
2023-09-05 17:14:17 +0200

    Added Intern card const.

--- src/cardbuild.js
+++ src/cardbuild.js
@@ -62,6 +62,34 @@ const engineerCardBuild = `
       </ul>
     </div>
 `;
+// Intern Card
+const internCardBuild = `
+    <!-- Intern Card  -->
+    <div class="card m-0 p-0 rounded" style="width: 18rem">
+      <!-- Div to hold both h5 and h6 with the same blue background -->
+      <div
+        class="bg-primary text-white m-0 p-2"
+        style="border-bottom: 5px solid black"
+      >
+        <h5 class="mb-0 text-center">Employee Name</h5>
+        <h6 class="mb-0 text-center">Employee Title</h6>
+      </div>
+      <!-- Separate card to hold the list items -->
+      <ul class="list-group list-group-flush m-0 p-4 border border-black">
+        <li class="list-group-item border border-secondary-subtle rounded-top">
+          ID:
+        </li>
+        <li class="list-group-item border border-secondary-subtle">
+          Email: <a href="mailto: "></a>
+        </li>
+        <li
+          class="list-group-item border border-secondary-subtle rounded-bottom"
+        >
+          School:
+        </li>
+      </ul>
+    </div>
+`;
 /******************************************/
 /* Function and class declarations */
 /******************************************/
