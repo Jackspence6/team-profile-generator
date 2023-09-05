78fdc930ee3d3cf44b75901cfc40de30bb19743c
Jackspence6 <jackspence123456@gmail.com>
2023-09-05 17:12:40 +0200

    Added Engineer card build const.

--- src/cardbuild.js
+++ src/cardbuild.js
@@ -6,6 +6,7 @@
 /* Global variables and constants */
 /******************************************/
 // Card Building code for employee cards
+// Manager Card
 const managerCardBuild = `
     <!-- Manager Card  -->
     <div class="card m-0 p-0 rounded" style="width: 18rem">
@@ -29,6 +30,34 @@ const managerCardBuild = `
           class="list-group-item border border-secondary-subtle rounded-bottom"
         >
           Office Number:
+        </li>
+      </ul>
+    </div>
+`;
+// Engineer Card
+const engineerCardBuild = `
+    <!-- Engineer Card  -->
+    <div class="card m-0 p-0 rounded" style="width: 18rem">
+      <!-- Div to hold both h5 and h6 with the same blue background -->
+      <div
+        class="bg-primary text-white m-0 p-2"
+        style="border-bottom: 5px solid black"
+      >
+        <h5 class="mb-0 text-center">Employee Name</h5>
+        <h6 class="mb-0 text-center">Employee Title</h6>
+      </div>
+      <!-- Separate card to hold the list items -->
+      <ul class="list-group list-group-flush m-0 p-4 border border-black">
+        <li class="list-group-item border border-secondary-subtle rounded-top">
+          ID:
+        </li>
+        <li class="list-group-item border border-secondary-subtle">
+          Email: <a href="mailto: "></a>
+        </li>
+        <li
+          class="list-group-item border border-secondary-subtle rounded-bottom"
+        >
+          Github: <a href="https://github.com/ " target="_blank"></a>
         </li>
       </ul>
     </div>
