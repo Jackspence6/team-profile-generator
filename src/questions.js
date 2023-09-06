1dd104049caa51308d61aba3bb46b9905dad3d2c
Jackspence6 <jackspence123456@gmail.com>
2023-09-06 11:42:40 +0200

    Updated the EMployee Questions for each role.

--- src/questions.js
+++ src/questions.js
@@ -1,54 +1,99 @@
-// User questions for command line Inquirer
-const mainQuestions = [
+// Manager questions
+const managerQuestions = [
+  {
+    name: "name",
+    type: "input",
+    message: "Please Provide the Manager's Full Name and Surname:",
+  },
+  {
+    name: "id",
+    type: "number",
+    message: "Please provide the Manager's Employee ID number:",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Please provide the Manager's Email Address:",
+  },
+  {
+    name: "phoneNumber",
+    type: "number",
+    message: "Please provide the Manager's Phone Number:",
+  },
+];
+
+// Additional employee Question
+const additionalEmployeeQuestion = [
+  {
+    name: "addEmployee",
+    type: "confirm",
+    message: "Do you want to add an additional Employee to the Team:",
+  },
+];
+
+// Employee Role question
+const employeeRoleQuestion = [
   {
     name: "role",
     type: "list",
     message: "Please select the employee's position:",
     choices: ["Manager", "Intern", "Engineer"],
   },
+];
+
+// Engineer questions
+const engineerQuestions = [
   {
     name: "name",
     type: "input",
-    message: "Please Provide your Full Name and Surname:",
+    message: "Please Provide the Engineer's Full Name and Surname:",
   },
   {
     name: "id",
     type: "number",
-    message: "Please provide you Employee ID number:",
+    message: "Please provide the Engineer's Employee ID number:",
   },
   {
     name: "email",
     type: "input",
-    message: "Please provide your Email Address:",
+    message: "Please provide the Engineer's Email Address:",
   },
-];
-
-// Manager additional question
-const managerQuestion = [
-  {
-    name: "phoneNumber",
-    type: "number",
-    message: "Please provide your Phone Number:",
-  },
-];
-
-// Engineer additional question
-const engineerQuestion = [
   {
     name: "githubUsername",
     type: "input",
-    message: "Please provide your Github Profile URL:",
+    message: "Please provide the Engineer's Github Profile URL:",
   },
 ];
 
-// Intern additional question
-const internQuestion = [
+// Intern questions
+const internQuestions = [
+  {
+    name: "name",
+    type: "input",
+    message: "Please Provide the Intern's Full Name and Surname:",
+  },
+  {
+    name: "id",
+    type: "number",
+    message: "Please provide the Intern's Employee ID number:",
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Please provide the Intern's Email Address:",
+  },
   {
     name: "school",
     type: "input",
-    message: "Please provide the name of the school you attended:",
+    message: "Please provide the name of the school the Intern attended:",
   },
 ];
 
 // Exporting questions for use on different files
-export { mainQuestions, managerQuestion, engineerQuestion, internQuestion };
+export {
+  managerQuestions,
+  additionalEmployeeQuestion,
+  employeeRoleQuestion,
+  engineerQuestions,
+  internQuestions,
+};
