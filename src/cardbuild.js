d1c3afd41d36d4244464ee53c9cab5dbe2e075a3
Jackspence6 <jackspence123456@gmail.com>
2023-09-08 14:46:03 +0200

    Updated internCardBuild function.

--- src/cardbuild.js
+++ src/cardbuild.js
@@ -63,7 +63,7 @@ const engineerCardBuild = (engineer) => `
     </div>
 `;
 // Intern Card
-const internCardBuild = `
+const internCardBuild = (intern) => `
     <!-- Intern Card  -->
     <div class="card m-0 p-0 rounded" style="width: 18rem">
       <!-- Div to hold both h5 and h6 with the same blue background -->
@@ -71,21 +71,21 @@ const internCardBuild = `
         class="bg-primary text-white m-0 p-2"
         style="border-bottom: 5px solid black"
       >
-        <h5 class="mb-0 text-center">Employee Name</h5>
-        <h6 class="mb-0 text-center">Employee Title</h6>
+        <h5 class="mb-0 text-center">${intern.getName()}</h5>
+        <h6 class="mb-0 text-center">${intern.getRole()}</h6>
       </div>
       <!-- Separate card to hold the list items -->
       <ul class="list-group list-group-flush m-0 p-4 border border-black">
         <li class="list-group-item border border-secondary-subtle rounded-top">
-          ID:
+          ID:  ${intern.getId()}
         </li>
         <li class="list-group-item border border-secondary-subtle">
-          Email: <a href="mailto: "></a>
+          Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a>
         </li>
         <li
           class="list-group-item border border-secondary-subtle rounded-bottom"
         >
-          School:
+          School:  ${intern.getSchool()}
         </li>
       </ul>
     </div>
