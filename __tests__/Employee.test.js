60949fc32b21f05f4e8321c5189603fcbdd62b45
Jackspence6 <jackspence123456@gmail.com>
2023-09-09 12:54:08 +0200

    Wrote a test for Employee.test.js.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,7 @@
 /******************************************/
 /* External dependencies */
 /******************************************/
-
+import { Employee } from "../lib/Employee.js";
 /******************************************/
 /* Mocks */
 /******************************************/
@@ -25,3 +25,23 @@
 /******************************************/
 /* Additional Test Suites */
 /******************************************/
+const testEmployee = new Employee("John Doe", "43", "johndoe@gmail.com");
+
+describe("Employee", () => {
+  describe("Creates a new Employee Object", () => {
+    it("Should add a name, id and email to the object", () => {
+      expect(testEmployee.name).toBe("John Doe");
+      expect(testEmployee.id).toBe(43);
+      expect(testEmployee.email).toBe("johndoe@gmail.com");
+    });
+    it("Should return the Employee name when invoked", () => {
+      expect(testEmployee.getName()).toBe("John Doe");
+    });
+    it("Should return the Employee Id when invoked", () => {
+      expect(testEmployee.getId()).toBe(43);
+    });
+    it("Should return the Employee Email when invoked", () => {
+      expect(testEmployee.getEmail()).toBe("johndoe@gmail.com");
+    });
+  });
+});
